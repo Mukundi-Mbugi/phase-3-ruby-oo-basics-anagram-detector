a5580e1ca6351c7158446832b895ec7b0ca0ee71
Elijah Mbugi <mukundielijah@gmail.com>
2022-06-09 12:59:04 +0300

    commit tree 1ebb82e7e250edd8d9e9cb64cab36c4f5ad06fb8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+  
+class Anagram
+
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name 
+    end   
+  
+    def match(array)
+      array.select do |word|
+        word.chars.sort == @name.chars.sort
+      end
+    end   
+  
+  end 
